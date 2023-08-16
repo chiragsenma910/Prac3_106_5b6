598320cb85f1f789294387bffdf358cbe4471ede
ravat <senmachiragkumar@gmail.com>
2023-08-16 22:03:25 +0530

    Practical 3

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Prac3_106_5b6"
+include(":app")
+ 
